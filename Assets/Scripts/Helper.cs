3255e6fd6fa1d009f730258d1e28125f4d2cbc39
Davinderpal Cheema <dcheema6@uwo.ca>
2020-02-24 20:45:16 -0500

    added save game system

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+public static class Helper
+{
+    public static string Serialize<T>(this T toSerialize)
+    {
+        XmlSerializer xmlS = new XmlSerializer(typeof(T));
+        StringWriter writer = new StringWriter();
+        xmlS.Serialize(writer, toSerialize);
+        return writer.ToString();
+    }
+
+    public static T Deserialize<T>(this string toDeserialize)
+    {
+        XmlSerializer xml = new XmlSerializer(typeof(T));
+        StringReader reader = new StringReader(toDeserialize);
+        return (T) xml.Deserialize(reader);
+    }
+}
---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { set; get; }
+    public SaveGame game;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+        Load();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetString("save", Helper.Serialize<SaveGame>(game));
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("save"))
+        {
+            game = Helper.Deserialize<SaveGame>(PlayerPrefs.GetString("save"));
+        }
+        else
+        {
+            game = new SaveGame();
+            Save();
+            Debug.Log("No save game found! Saving...");
+        }
+    }
+}

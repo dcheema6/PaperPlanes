4234722a111fad251e89d7281db31ecc322b659e
Davinderpal Cheema <dcheema6@uwo.ca>
2020-02-24 19:58:02 -0500

    adding art assets, added peload page and a couple menus

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f;
+
+    public Transform colorPanel;
+    public Transform trailPanel;
+    
+    void Start()
+    {
+        // Grab the only one CanvasGroup in the scene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start with a white screen
+        fadeGroup.alpha = 1;
+    
+        // Add button on-click events to shop buttons
+        InitShop();
+    }
+
+    void Update()
+    {
+        // Fade-in
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+    }
+
+    private void InitShop()
+    {
+        // Check references
+        if (colorPanel == null || trailPanel == null)
+            Debug.Log("Color/Trail panel refence(s) has not been assigned in inspector");
+
+        // Add event listerners to every child of color/trail panel
+        int i = 0;
+        foreach (Transform t in colorPanel)
+        {
+            int currInd = i;
+            Button b = t.GetComponent<Button> ();
+            b.onClick.AddListener(() => OnColorSelect(currInd));
+            i++;
+        }
+
+        i = 0;
+        foreach (Transform t in trailPanel)
+        {
+            int currInd = i;
+            Button b = t.GetComponent<Button> ();
+            b.onClick.AddListener(() => OnTrailSelect(currInd));
+            i++;
+        }
+    }
+
+    // Buttton Controls
+    public void OnPlay()
+    {
+        Debug.Log("Play");
+    }
+
+    public void OnExit()
+    {
+        Debug.Log("Exit");
+    }
+    
+    private void OnColorSelect(int i)
+    {
+        Debug.Log("Selecting color: " + i);
+    }
+    
+    private void OnTrailSelect(int i)
+    {
+        Debug.Log("Selecting trail: " + i);
+    }
+
+    public void OnColorSet()
+    {
+        Debug.Log("Set color");
+    }
+
+    public void OnTrailSet()
+    {
+        Debug.Log("Trail color");
+    }
+}
---
+++ Assets/Scripts/PreLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class PreLoader : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float loadTime;
+    private float minLogoTime = 3.0f; // Minimum scene time
+
+    private void Start()
+    {
+        // Grab the only one CanvasGroup in the scene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start with a white screen
+        fadeGroup.alpha = 1;
+
+        // Pre load the game
+        // Here
+
+        if (Time.time < minLogoTime)
+            loadTime = minLogoTime;
+        else
+            loadTime = Time.time;
+    }
+
+    private void Update()
+    {
+        // Fade-in
+        if (Time.time < minLogoTime)
+            fadeGroup.alpha = 1 - Time.time;
+        // Fade-out 
+        else if (loadTime != 0)
+        {
+            fadeGroup.alpha = Time.time - minLogoTime;
+            if (fadeGroup.alpha >= 1)
+            {
+                EditorSceneManager.LoadScene("Menu");
+            }
+        }
+    }
+}
